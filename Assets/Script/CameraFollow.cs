4c5c92100b1de22b68dc73ccb2ec9a239243b5d1
CodeWCBrandon <christian.brandon@binus.ac.id>
2026-02-21 19:47:31 +0700

    Implementing Camera follow the player with damping

---
+++ Assets/Script/CameraFollow.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    [SerializeField] public Transform target;
+    [SerializeField] public float smoothIntensity = 0.3f;
+    [SerializeField] public Vector3 offset = new Vector3(0, 0, -10);
+    
+
+    private Vector3 velocity = Vector3.zero;
+    void LateUpdate()
+    {
+        if (target != null)
+        {
+            Vector3 targetPosition = target.position + offset;
+            transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothIntensity);
+        }
+    }
+}
