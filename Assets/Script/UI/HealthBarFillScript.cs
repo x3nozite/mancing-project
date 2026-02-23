1915cd8c985bf631c747877e99f48ac3e8c15e81
x3nozite <xeno.zite3@gmail.com>
2026-02-23 13:11:55 +0700

    Health Bar

---
+++ Assets/Script/UI/HealthBarFillScript.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class HealthBarFillScript : MonoBehaviour
+{
+    public float maxHealth;
+    public float currentHealth;
+    private float percent = 1;
+    private float maxMiddleWidth;
+
+    [SerializeField] private RectTransform startRect;
+    [SerializeField] private RectTransform middleRect;
+    [SerializeField] private RectTransform endRect;
+
+    void Start()
+    {
+        maxMiddleWidth = middleRect.sizeDelta.x;
+    }
+
+    void Update()
+    {
+        percent = currentHealth / maxHealth;
+        setRemainingHealth();
+    }
+
+    public void setRemainingHealth()
+    {
+        middleRect.sizeDelta = new Vector2(maxMiddleWidth * percent, middleRect.sizeDelta.y);
+        endRect.anchoredPosition = new Vector2(middleRect.anchoredPosition.x + middleRect.sizeDelta.x, endRect.anchoredPosition.y);
+    }
+}
