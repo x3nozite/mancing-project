ffe0910e60524b49697ad195c652143c61739c9c
x3nozite <xeno.zite3@gmail.com>
2026-02-24 21:12:23 +0700

    HP, EXP Bar, Buff Icons, Inventory UI

--- Assets/Script/UI/HealthBarFillScript.cs
+++ Assets/Script/UI/HealthBarFillScript.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthBarFillScript : MonoBehaviour
 {
@@ -8,9 +9,15 @@ public class HealthBarFillScript : MonoBehaviour
     private float percent = 1;
     private float maxMiddleWidth;
 
+    [Header("Health")]
     [SerializeField] private RectTransform startRect;
     [SerializeField] private RectTransform middleRect;
     [SerializeField] private RectTransform endRect;
+
+    [Header("EXP")]
+    [SerializeField] private Image expCircle;
+    [SerializeField] private float currentEXP;
+    [SerializeField] private float EXPUntilNextLevel;
 
     void Start()
     {
@@ -22,11 +29,17 @@ public class HealthBarFillScript : MonoBehaviour
         percent = currentHealth / maxHealth;
         percent = Math.Min(percent, 1);
         setRemainingHealth();
+        setEXPCircle();
     }
 
     public void setRemainingHealth()
     {
         middleRect.sizeDelta = new Vector2(maxMiddleWidth * percent, middleRect.sizeDelta.y);
         endRect.anchoredPosition = new Vector2(middleRect.anchoredPosition.x + middleRect.sizeDelta.x, endRect.anchoredPosition.y);
+    }
+
+    public void setEXPCircle()
+    {
+        expCircle.fillAmount = currentEXP / EXPUntilNextLevel;
     }
 }
