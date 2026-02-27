40e1f0c96c079230c4164ea322fed8aa1eaa4e07
x3nozite <xeno.zite3@gmail.com>
2026-02-27 22:31:09 +0700

    Improve fishing line curvy path

--- Assets/Script/Tools/RodLine.cs
+++ Assets/Script/Tools/RodLine.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 
 public class RodLine : MonoBehaviour
@@ -18,14 +17,14 @@ public class RodLine : MonoBehaviour
 
     void Update()
     {
-        DrawRodLine();
+        DrawLine();
     }
     void DrawRodLine()
     {
         Vector3 p0 = rodTip.transform.position;
         
         Vector3 p2 = hook.transform.position;
-        Vector3 p1 = new Vector3(p0.x, Math.Min(p0.y, p2.y), p0.z);
+        Vector3 p1 = new Vector3(p0.x, Mathf.Min(p0.y, p2.y), p0.z);
 
         for (int i = 0; i < segments; i++)
         {
@@ -33,5 +32,28 @@ public class RodLine : MonoBehaviour
             Vector3 point = Mathf.Pow(1 - t, 2) * p0 + 2 * (1 - t) * t * p1 + Mathf.Pow(t, 2) * p2;
             lineRenderer.SetPosition(i, point);
         }
+    }
+
+    void DrawLine()
+    {
+        Vector3[] points = new Vector3[segments];
+
+        Vector3 relativeRodPos = rodTip.transform.position - hook.transform.position;
+
+        float xSq = relativeRodPos.x * relativeRodPos.x;
+
+        float a = (xSq != 0) ? relativeRodPos.y / xSq : 0;
+
+        for (int i = 0; i < segments; i++)
+        {
+            float t = i / (float)(segments - 1);
+            float x = t * relativeRodPos.x;
+
+            float y = a * (x * x);
+
+            points[i] = new Vector3(x, y, 0) + hook.transform.position;
+        }
+
+        lineRenderer.SetPositions(points);
     }
 }
