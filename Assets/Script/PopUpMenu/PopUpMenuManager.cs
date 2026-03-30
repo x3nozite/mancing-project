14e4f19aa9c48e9b72a557e00accecae9f80d7b9
xenozite <xeno.zite3@gmail.com>
2026-03-30 11:44:28 +0700

    Replace placeholder with the idle minigame

--- Assets/Script/PopUpMenu/PopUpMenuManager.cs
+++ Assets/Script/PopUpMenu/PopUpMenuManager.cs
@@ -4,126 +4,127 @@ using UnityEngine.SceneManagement;
 
 public class PopUpMenuManager : MonoBehaviour
 {
-    public static PopUpMenuManager Instance;
-    private GameObject currentPrimaryPopUp;
-    private GameObject currentPrimaryPopUpPrefab;
-    private Stack<GameObject> modals = new Stack<GameObject>();
-    private List<GameObject> overlayPopUps = new List<GameObject>();
-    [SerializeField] private Transform PopUpRoot;
-    
-    void Awake()
-    {
-        if (Instance != null && Instance != this)
-        {
-            Destroy(gameObject);
-        }
-        SceneManager.sceneLoaded += OnSceneLoaded;
-        Instance = this;
-        DontDestroyOnLoad(gameObject);
-    }
+  public static PopUpMenuManager Instance;
+  private GameObject currentPrimaryPopUp;
+  private GameObject currentPrimaryPopUpPrefab;
+  private Stack<GameObject> modals = new Stack<GameObject>();
+  private List<GameObject> overlayPopUps = new List<GameObject>();
+  [SerializeField] private Transform PopUpRoot;
 
-    public void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+  void Awake()
+  {
+    if (Instance != null && Instance != this)
     {
-        Transform canvas = GameObject.FindGameObjectWithTag("PopUpRoot").transform;
-        if (canvas != null) {
-            SetPopUpRoot(canvas);
-        }
+      Destroy(gameObject);
     }
+    SceneManager.sceneLoaded += OnSceneLoaded;
+    Instance = this;
+    DontDestroyOnLoad(gameObject);
+  }
 
-    public void SetPopUpRoot(Transform newRoot)
+  public void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+  {
+    Transform canvas = GameObject.FindGameObjectWithTag("PopUpRoot").transform;
+    if (canvas != null)
     {
-        PopUpRoot = newRoot;
+      SetPopUpRoot(canvas);
     }
+  }
 
-    public GameObject OpenPrimaryPopUpMenu(GameObject menu)
-    {
-        if(currentPrimaryPopUp != null && currentPrimaryPopUpPrefab == menu)
-        {
-            return currentPrimaryPopUp;
-        }
-        else if(currentPrimaryPopUp != null)
-        {
-            ClosePrimaryPopUpMenu(currentPrimaryPopUp);
-        }
-        currentPrimaryPopUpPrefab = menu;
-        currentPrimaryPopUp = Instantiate(menu, PopUpRoot);
-        return currentPrimaryPopUp;
-    }
+  public void SetPopUpRoot(Transform newRoot)
+  {
+    PopUpRoot = newRoot;
+  }
 
-    public GameObject OpenPrimaryPopUpMenu(GameObject menu, Canvas canvas)
+  public GameObject OpenPrimaryPopUpMenu(GameObject menu)
+  {
+    if (currentPrimaryPopUp != null && currentPrimaryPopUpPrefab == menu)
     {
-        if (currentPrimaryPopUp != null && currentPrimaryPopUpPrefab == menu)
-        {
-            return currentPrimaryPopUp;
-        }
-        else if (currentPrimaryPopUp != null)
-        {
-            ClosePrimaryPopUpMenu(currentPrimaryPopUp);
-        }
-        currentPrimaryPopUpPrefab = menu;
-        currentPrimaryPopUp = Instantiate(menu, canvas.transform);
-        return currentPrimaryPopUp;
+      return currentPrimaryPopUp;
     }
+    else if (currentPrimaryPopUp != null)
+    {
+      ClosePrimaryPopUpMenu(currentPrimaryPopUp);
+    }
+    currentPrimaryPopUpPrefab = menu;
+    currentPrimaryPopUp = Instantiate(menu, PopUpRoot);
+    return currentPrimaryPopUp;
+  }
 
-    public void ClosePrimaryPopUpMenu(GameObject menu)
+  public GameObject OpenPrimaryPopUpMenu(GameObject menu, Canvas canvas)
+  {
+    if (currentPrimaryPopUp != null && currentPrimaryPopUpPrefab == menu)
     {
-        if (currentPrimaryPopUp != null)
-        {
-            Destroy(menu);
-            currentPrimaryPopUp = null;
-        }
+      return currentPrimaryPopUp;
     }
+    else if (currentPrimaryPopUp != null)
+    {
+      ClosePrimaryPopUpMenu(currentPrimaryPopUp);
+    }
+    currentPrimaryPopUpPrefab = menu;
+    currentPrimaryPopUp = Instantiate(menu, canvas.transform);
+    return currentPrimaryPopUp;
+  }
 
-    public GameObject OpenBlockingModal(GameObject modal)
+  public void ClosePrimaryPopUpMenu(GameObject menu)
+  {
+    if (currentPrimaryPopUp != null)
     {
-        GameObject newModal = Instantiate(modal, PopUpRoot);
-        modals.Push(newModal);
-        return newModal;
+      Destroy(menu);
+      currentPrimaryPopUp = null;
     }
+  }
 
-    public GameObject OpenBlockingModal(GameObject modal, Canvas canvas)
-    {
-        GameObject newModal = Instantiate(modal, canvas.transform);
-        modals.Push(newModal);
-        return newModal;
-    }
+  public GameObject OpenBlockingModal(GameObject modal)
+  {
+    GameObject newModal = Instantiate(modal, PopUpRoot);
+    modals.Push(newModal);
+    return newModal;
+  }
 
-    public void CloseBlockingModal()
-    {
-        if(modals.Count > 0)
-        {
-            GameObject top = modals.Pop();
-            Destroy(top);
-        }
-    }
+  public GameObject OpenBlockingModal(GameObject modal, Canvas canvas)
+  {
+    GameObject newModal = Instantiate(modal, canvas.transform);
+    modals.Push(newModal);
+    return newModal;
+  }
 
-    public GameObject OpenOverlayPopUpMenu(GameObject menu)
+  public void CloseBlockingModal()
+  {
+    if (modals.Count > 0)
     {
-        GameObject activePopUp = Instantiate(menu, PopUpRoot);
-        overlayPopUps.Add(activePopUp);
-        return activePopUp;
+      GameObject top = modals.Pop();
+      Destroy(top);
     }
+  }
 
-    public GameObject OpenOverlayPopUpMenu(GameObject menu, Canvas canvas)
-    {
-        GameObject activePopUp = Instantiate(menu, canvas.transform);
-        overlayPopUps.Add(activePopUp);
-        return activePopUp;
-    }
+  public GameObject OpenOverlayPopUpMenu(GameObject menu)
+  {
+    GameObject activePopUp = Instantiate(menu, PopUpRoot);
+    overlayPopUps.Add(activePopUp);
+    return activePopUp;
+  }
 
-    public GameObject OpenOverlayPopUpMenu(GameObject menu, Transform parent)
-    {
-        GameObject activePopUp = Instantiate(menu, parent);
-        overlayPopUps.Add(activePopUp);
-        return activePopUp;
-    }
+  public GameObject OpenOverlayPopUpMenu(GameObject menu, Canvas canvas)
+  {
+    GameObject activePopUp = Instantiate(menu, canvas.transform);
+    overlayPopUps.Add(activePopUp);
+    return activePopUp;
+  }
 
-    public void CloseOverlayPopUpMenu(GameObject menu)
+  public GameObject OpenOverlayPopUpMenu(GameObject menu, Transform parent)
+  {
+    GameObject activePopUp = Instantiate(menu, parent);
+    overlayPopUps.Add(activePopUp);
+    return activePopUp;
+  }
+
+  public void CloseOverlayPopUpMenu(GameObject menu)
+  {
+    if (overlayPopUps.Contains(menu))
     {
-        if (overlayPopUps.Contains(menu))
-        {
-            overlayPopUps.Remove(menu);
-            Destroy(menu);
-        }
+      overlayPopUps.Remove(menu);
+      Destroy(menu);
     }
+  }
 }
--- Assets/Script/Tools/FishingRodScript.cs
+++ Assets/Script/Tools/FishingRodScript.cs
@@ -4,102 +4,107 @@ using UnityEngine.UI;
 
 public class FishingRodScript : MonoBehaviour
 {
-    [SerializeField] private FishingRodData fishingRod;
-    [SerializeField] private SpriteRenderer spriteRenderer;
-    [SerializeField] private Player player;
-    
-    public Canvas worldCanvas;
+  [SerializeField] private FishingRodData fishingRod;
+  [SerializeField] private SpriteRenderer spriteRenderer;
+  [SerializeField] private Player player;
 
-    public GameObject castingGauge;
-    private GameObject currentGauge;
-    private CastingRodGauge gauge;
+  public Canvas worldCanvas;
 
-    [Header("Fishing Rod Compartments")]
-    [SerializeField] private GameObject rodTip;
-    [SerializeField] private GameObject hook;
+  public GameObject castingGauge;
+  private GameObject currentGauge;
+  private CastingRodGauge gauge;
 
-    [Header("Minigames")]
-    [SerializeField] private GameObject IdleMinigame;
-    private GameObject currentMinigame;
+  [Header("Fishing Rod Compartments")]
+  [SerializeField] private GameObject rodTip;
+  [SerializeField] private GameObject hook;
 
-    public FishingState state = FishingState.Idle;
+  [Header("Minigames")]
+  [SerializeField] private GameObject IdleMinigame;
+  private GameObject currentMinigame;
 
-    void Awake()
+  public FishingState state = FishingState.Idle;
+
+  void Awake()
+  {
+    spriteRenderer.sprite = fishingRod.FishingRodSprite;
+    transform.SetParent(player.transform);
+    transform.localPosition = new Vector2(0.4f, 0f);
+    transform.rotation = Quaternion.Euler(0f, 0f, -45f);
+
+  }
+
+  void Update()
+  {
+    if (Input.GetKeyDown(KeyCode.F) && currentGauge == null && state == FishingState.Idle)
     {
-        spriteRenderer.sprite = fishingRod.FishingRodSprite;
-        transform.SetParent(player.transform);
-        transform.localPosition = new Vector2(0.4f, 0f);
-        transform.rotation = Quaternion.Euler(0f, 0f, -45f);
-        
+      currentGauge = PopUpMenuManager.Instance.OpenOverlayPopUpMenu(castingGauge, worldCanvas);
+      gauge = currentGauge.GetComponent<CastingRodGauge>();
+      gauge.onCastConfirmed += HandleCastConfirmed;
+      state = FishingState.Casting;
     }
 
-    void Update()
+    // TEMPORARY. ONLY FOR TESTING
+    if (state == FishingState.Waiting)
     {
-        if (Input.GetKeyDown(KeyCode.F) && currentGauge == null && state == FishingState.Idle)
-        {
-            currentGauge = PopUpMenuManager.Instance.OpenOverlayPopUpMenu(castingGauge, worldCanvas);
-            gauge = currentGauge.GetComponent<CastingRodGauge>();
-            gauge.onCastConfirmed += HandleCastConfirmed;
-            state = FishingState.Casting;
-        }
-
-        // TEMPORARY. ONLY FOR TESTING
-        if(state == FishingState.Waiting)
-        {
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                PopUpMenuManager.Instance.CloseOverlayPopUpMenu(currentMinigame);
-                state = FishingState.Idle;
-                ResetRod();
-            }
-        }
+      if (Input.GetKeyDown(KeyCode.Space))
+      {
+        PopUpMenuManager.Instance.CloseOverlayPopUpMenu(currentMinigame);
+        state = FishingState.Idle;
+        ResetRod();
+      }
     }
+  }
 
-    public void SetItem(FishingRodData item)
-    {
-        fishingRod = item;
-        spriteRenderer.sprite = fishingRod.FishingRodSprite;
-    }
+  public void SetItem(FishingRodData item)
+  {
+    fishingRod = item;
+    spriteRenderer.sprite = fishingRod.FishingRodSprite;
+  }
 
-    void HandleCastConfirmed(float accuracy)
-    {
-        CastRod(accuracy);
-        gauge.onCastConfirmed -= HandleCastConfirmed; 
-        currentGauge = null;
-    }
+  void HandleCastConfirmed(float accuracy)
+  {
+    CastRod(accuracy);
+    gauge.onCastConfirmed -= HandleCastConfirmed;
+    currentGauge = null;
+  }
 
-    void CastRod(float force)
-    {
-        hook.transform.SetParent(null);
-        hook.transform.position = rodTip.transform.position;
-        Hook hookScript = hook.GetComponentInChildren<Hook>();
-        hookScript.Launch(force, OnHookCastFinished);
-    }
+  void CastRod(float force)
+  {
+    hook.transform.SetParent(null);
+    hook.transform.position = rodTip.transform.position;
+    Hook hookScript = hook.GetComponentInChildren<Hook>();
+    hookScript.Launch(force, OnHookCastFinished);
+  }
 
-    void ResetRod()
-    {
-        hook.transform.SetParent(gameObject.transform);
-        hook.transform.position = rodTip.transform.position;
-    }
+  void ResetRod()
+  {
+    hook.transform.SetParent(gameObject.transform);
+    hook.transform.position = rodTip.transform.position;
+  }
 
-    void OnHookCastFinished()
-    {
-        if (state != FishingState.Casting) return;
+  void OnHookCastFinished()
+  {
+    if (state != FishingState.Casting) return;
 
-        state = FishingState.Waiting;
-        StartWaitingMinigame();
-    }
+    state = FishingState.Waiting;
+    StartWaitingMinigame();
+  }
 
-    void StartWaitingMinigame()
-    {
-        currentMinigame = PopUpMenuManager.Instance.OpenOverlayPopUpMenu(IdleMinigame);
-    }
+  void StartWaitingMinigame()
+  {
+    currentMinigame = PopUpMenuManager.Instance.OpenOverlayPopUpMenu(IdleMinigame, player.transform);
+    currentMinigame.transform.position = new Vector3(
+        currentMinigame.transform.position.x,
+        1f,
+        currentMinigame.transform.position.z
+    );
+  }
 }
 
 public enum FishingState
 {
-    Idle,
-    Casting,
-    Waiting,
-    Reeling
+  Idle,
+  Casting,
+  Waiting,
+  Reeling
 }
